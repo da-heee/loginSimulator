4f16492bd7dd3f2aa95d8e5312ecaa1c95dcdd8f
da-heee <fjqmfkdlqm__584>
2020-10-11 20:36:30 +0900

    "can change background color"

--- app/src/main/java/com/example/loginsimulator/MainActivity.java
+++ app/src/main/java/com/example/loginsimulator/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.loginsimulator;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,8 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         final String rightId = "abc";
-        final String rightPassword = "abc";
+        final String rightPassword = "1234";
         // 아이디랑 비밀번호 설정
 
         final EditText editText = findViewById(R.id.ET_Id);
@@ -34,12 +39,35 @@ public class MainActivity extends AppCompatActivity {
                 if (inputId.trim().equals(rightId) && inputPassword.trim().equals(rightPassword)){
                     // 입력된 아이디랑 비밀번호가 맞는지 확인
 
+                    ConstraintLayout layout_main = findViewById(R.id.layoutMain);
+                    Drawable.ConstantState cid = layout_main.getBackground().getConstantState();
+
                     Intent intent = new Intent(MainActivity.this,subActivity.class);
                     startActivityForResult(intent,101);
-                } else {Toast.makeText(getApplicationContext(), "Wrong!", Toast.LENGTH_SHORT).show();
 
+                } else {Toast.makeText(getApplicationContext(), "Wrong!", Toast.LENGTH_SHORT).show();
             }
         }
     });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        ConstraintLayout layout_main = findViewById(R.id.layoutMain);
+
+        if (requestCode == 101 && resultCode == RESULT_OK && data != null) {
+
+            String returnColor = data.getStringExtra("RETURN_COLOR");
+
+            if (returnColor.equals("almost")) {
+                layout_main.setBackgroundResource(R.drawable.almost);
+            } else if (returnColor.equals("moor")) {
+                layout_main.setBackgroundResource(R.drawable.moor);
+            } else if (returnColor.equals("calmdarya")) {
+                layout_main.setBackgroundResource(R.drawable.calmdarya);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/loginsimulator/subActivity.java
+++ app/src/main/java/com/example/loginsimulator/subActivity.java
@@ -1,14 +1,26 @@
 package com.example.loginsimulator;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.PaintDrawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import static java.util.logging.Logger.global;
+
 public class subActivity extends AppCompatActivity implements View.OnClickListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,23 +31,56 @@ public class subActivity extends AppCompatActivity implements View.OnClickListen
         Button buttonMoor = findViewById(R.id.BTN_Moor);
         Button buttonCalmdarya = findViewById(R.id.BTN_Calmdarya);
 
+
         buttonAlmost.setOnClickListener(this);
         buttonMoor.setOnClickListener(this);
         buttonCalmdarya.setOnClickListener(this);
+
     }
+
 
     @Override
     public void onClick(View view) {
+
+        ConstraintLayout layout_sub = findViewById(R.id.layoutSub);
+
         int cid = view.getId();
 
         if (cid == R.id.BTN_Almost) {
-            Toast.makeText(getApplicationContext(), "Almost", Toast.LENGTH_SHORT).show();}
-        if (cid == R.id.BTN_Moor) {
-            Toast.makeText(getApplicationContext(), "Moor", Toast.LENGTH_SHORT).show();}
-        if (cid == R.id.BTN_Calmdarya) {
-            Toast.makeText(getApplicationContext(), "Calmdarya", Toast.LENGTH_SHORT).show();}
+            layout_sub.setBackgroundResource(R.drawable.almost);
+        } else if (cid == R.id.BTN_Moor) {
+            layout_sub.setBackgroundResource(R.drawable.moor);
+        }else if (cid == R.id.BTN_Calmdarya) {
+            layout_sub.setBackgroundResource(R.drawable.calmdarya);
+        }
 
     }
 
+    @Override
+    public void onBackPressed() {
+        // 뒤로가기 버튼 눌렀을 때 실행되는 코드
+
+        ConstraintLayout layout_sub = findViewById(R.id.layoutSub);
+
+        Drawable.ConstantState cid = layout_sub.getBackground().getConstantState();
+
+        Intent intent = new Intent();
+        // Intent()안에 아무것도 안 적어도 되는 이유
+        // 뒤로가기 하면 자동으로 원래 있던 activity로 돌아가서
+
+        if (cid.equals(getResources().getDrawable(R.drawable.almost).getConstantState())) {
+            intent.putExtra("RETURN_COLOR", "almost");
+            // drawable 비교에는 == 못 씀
+        } else if ((cid.equals(getResources().getDrawable(R.drawable.moor).getConstantState()))) {
+            intent.putExtra("RETURN_COLOR", "moor");
+        } else if ((cid.equals(getResources().getDrawable(R.drawable.calmdarya).getConstantState()))) {
+            intent.putExtra("RETURN_COLOR", "calmdarya");
+        }
+
+        setResult(RESULT_OK, intent);
+        // 멀쩡하다는 거 보증
+
+        super.onBackPressed(); // finish()의 효과?
+    }
 
 }
